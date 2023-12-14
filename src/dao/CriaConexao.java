81f36bd3155cdef8223c3beda7ea961b95331200
Gustavo Eufrazio <129990762+GustavoEufrazio@users.noreply.github.com>
2023-12-13 21:10:45 -0300

    Update CriaConexao.java

--- src/dao/CriaConexao.java
+++ src/dao/CriaConexao.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author LAB_ETESC
- */
+
 public class CriaConexao {
     
     public static Connection getconexao() throws SQLException{
